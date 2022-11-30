d3bf204a8669ef2e3d4bc17883ff16d39660f2ab
ansari <makansari.android@gmail.com>
2022-11-30 16:27:32 +0530

    animations

--- DBSBroadcastReceiver/app/src/main/java/com/example/dbsbroadcastreceiver/MainActivity.kt
+++ DBSBroadcastReceiver/app/src/main/java/com/example/dbsbroadcastreceiver/MainActivity.kt
@@ -2,17 +2,44 @@ package com.example.dbsbroadcastreceiver
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sp : SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mytext = findViewById<TextView>(R.id.myTextview)
+
+//        var anim = AnimationUtils.loadAnimation(this, R.anim.blink)
+//        mytext.startAnimation(anim)
+
+        mytext.setOnClickListener {
+            var i = Intent(this, SecondAcrivity::class.java)
+
+            startActivity(i)
+
+            overridePendingTransition(R.anim.blink,
+                R.anim.blink);
+        }
+
+
+
+        var usernameEditText = findViewById<EditText>(R.id.editTextUserName)
+        var submitButton = findViewById<Button>(R.id.buttonSubmit)
+        Log.i("mytag", "activity created")
         
         var myIntentFilter = IntentFilter("android.provider.Telephony.SMS_RECEIVED")
         var myReceiver = MyReceiver()
@@ -34,5 +61,48 @@ class MainActivity : AppCompatActivity() {
             sendBroadcast(myIntent)
         }*/
 
+
+
+        sp = getSharedPreferences("myfile", MODE_PRIVATE)
+
+      usernameEditText.setText(sp.getString("keyun",""))
+
+
+        submitButton.setOnClickListener {
+            var username = usernameEditText.text.toString()
+            var editor = sp.edit()
+            editor.putString("keyun", username)
+            editor.commit()
+        }
+
+
+
+
     }
+
+/*    override fun onStart() {
+        super.onStart()
+        Log.i("mytag", "activity start")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("mytag", "activity resume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("mytag", "activity pause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("mytag", "activity stop")
+
+    }*/
 }
--- DBSBroadcastReceiver/app/src/main/java/com/example/dbsbroadcastreceiver/MyReceiver.kt
+++ DBSBroadcastReceiver/app/src/main/java/com/example/dbsbroadcastreceiver/MyReceiver.kt
@@ -3,14 +3,18 @@ package com.example.dbsbroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.speech.tts.TextToSpeech
 import android.telephony.SmsMessage
 import android.util.Log
 import android.widget.Toast
+import java.util.*
 
-class MyReceiver : BroadcastReceiver() {
-
+class MyReceiver : BroadcastReceiver(),TextToSpeech.OnInitListener {
+    lateinit var tts : TextToSpeech
+    lateinit var  myMessage : String
     override fun onReceive(ctx: Context?, intent: Intent?) {
        Toast.makeText(ctx, "sms received.... ", Toast.LENGTH_LONG).show()
+         tts = TextToSpeech(ctx, this)
 
         var bundle = intent?.extras
         if(bundle != null){
@@ -20,7 +24,8 @@ class MyReceiver : BroadcastReceiver() {
             for( i in pdusObj.indices){
                 val currentMessage = SmsMessage.createFromPdu(pdusObj[i] as ByteArray)
 
-                val myMessage = currentMessage.displayMessageBody
+                 myMessage = currentMessage.displayMessageBody
+                tts.speak(myMessage, TextToSpeech.QUEUE_FLUSH, null,"")
                 val receiverPh = currentMessage.originatingAddress
 
                 Toast.makeText(ctx,"message is $myMessage and sender is : $receiverPh",Toast.LENGTH_LONG).show()
@@ -29,15 +34,26 @@ class MyReceiver : BroadcastReceiver() {
             }
 
 
+        }
 
+    }
 
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // set US English as language for tts
+            val result = tts!!.setLanguage(Locale.US)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            } else {
+                Log.e("TTS","The Language specified is  supported!")
+                tts.speak(myMessage, TextToSpeech.QUEUE_FLUSH, null,"")
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
         }
 
 
-
-
-       /* var i = Intent(ctx, SecondAcrivity::class.java)
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        ctx?.startActivity(i)*/
     }
 }
